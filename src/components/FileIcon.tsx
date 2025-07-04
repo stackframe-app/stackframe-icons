b08d6d3a5f22667bce3849ee9215dc8cb41dc2a6
TechG <errorersplai@gmail.com>
2025-07-04 22:44:50 +0200

    commit

--- src/components/FileIcon.tsx
+++ src/components/FileIcon.tsx
@@ -8,20 +8,20 @@ type Props = {
 } & React.SVGProps<SVGSVGElement>;
 
 export const FileIcon: FC<Props> = ({ name, isFolder = false, ...svgProps }) => {
-  let iconName = "default-file";
+  let iconName = "default_file";
 
   if (isFolder) {
     const folder = folders.supported.find(f =>
       (f.extensions ?? []).some(ext => ext === name.toLowerCase())
     );
-    iconName = folder ? `folder_type_${folder.icon}` : "default-folder";
+    iconName = folder ? `folder_type_${folder.icon}` : "default_folder";
   } else {
     const parts = name.split(".");
     for (let i = 0; i < parts.length; i++) {
       const extensionCandidate = parts.slice(i).join(".").toLowerCase();
 
       const match = extensions.supported.find(e =>
-        (e.extensions ?? []).some(ext => ext === extensionCandidate)
+        ([...(e.extensions ?? []), ...(e.languages?.map(lang => lang.defaultExtension) ?? [])]).some(ext => ext === extensionCandidate)
       );
 
       if (match) {
@@ -31,7 +31,6 @@ export const FileIcon: FC<Props> = ({ name, isFolder = false, ...svgProps }) => 
     }
   }
 
-  const SvgIcon = iconMap[iconName] || iconMap["default-file"];
-
+  const SvgIcon = iconMap[iconName] || iconMap["default_file"];
   return <SvgIcon {...svgProps} />;
 };
